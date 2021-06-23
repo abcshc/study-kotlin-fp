56f8f4ed0d4c5cdea427436fe869520de1762678
Heechul So <thrrkthrrk@gmail.com>
2021-06-24 08:16:26 +0900

    Add recursive tests

--- lib/src/main/kotlin/study/kotlin/fp/Recursion.kt
+++ lib/src/main/kotlin/study/kotlin/fp/Recursion.kt
@@ -3,35 +3,58 @@ package study.kotlin.fp
 import java.math.BigInteger
 
 object Recursion {
-    fun fibonacciNumbers(n: Int): BigInteger {
-        return when (n) {
-            0 -> BigInteger.ZERO
-            1 -> BigInteger.ONE
-            else -> fibonacciNumbers(n - 1) + fibonacciNumbers(n - 2)
+    fun fibonacciNumbers(n: Int): BigInteger = when (n) {
+        0 -> BigInteger.ZERO
+        1 -> BigInteger.ONE
+        else -> fibonacciNumbers(n - 1) + fibonacciNumbers(n - 2)
+    }
+    
+    fun power(base: Int, exponent: Int): Int = when (exponent) {
+        0 -> 1
+        else -> base * power(base, exponent - 1)
+    }
+    
+    fun factorial(n: Int): Int = when (n) {
+        0 -> 1
+        else -> factorial(n - 1) * n
+    }
+    
+    fun List<Int>.maximum(): Int = when (this.size) {
+        0 -> error("empty list")
+        1 -> this.first()
+        else -> kotlin.math.max(this[0], this.subList(1, this.size).maximum())
+    }
+    
+    fun <E> List<E>.reverse(): List<E> = when (this.size) {
+        0 -> throw error("empty list")
+        1 -> this
+        else -> mutableListOf<E>().apply {
+            add(this@reverse.last())
+            addAll(this@reverse.subList(0, this@reverse.size - 1).reverse())
         }
     }
     
-    fun power(base: Int, exponent: Int): Int {
-        return when (exponent) {
-            0 -> 1
-            else -> base * power(base, exponent - 1)
+    fun convertDecimalToBinaryString(decimal: Int): String = when (decimal) {
+        0 -> "0"
+        1 -> "1"
+        else -> convertDecimalToBinaryString(decimal / 2) + (decimal % 2).toString()
+    }
+    
+    fun replicate(count: Int, target: Int): List<Int> = when (count) {
+        0 -> listOf()
+        1 -> listOf(target)
+        else -> mutableListOf<Int>().apply {
+            add(target)
+            addAll(replicate(count - 1, target))
         }
     }
     
-    fun factorial(n: Int): Int {
-        return when (n) {
-            0 -> 1
-            else -> factorial(n - 1) * n
-        }
-    }
-    
-    fun List<Int>.max(): Int {
-        return when (this.size) {
-            0 -> throw EmptyListException()
-            1 -> this.first()
-            else -> kotlin.math.max(this[0], this.subList(1, this.size).max())
+    fun <E> take(count: Int, list: List<E>): List<E> = when (count) {
+        0 -> listOf()
+        1 -> listOf(list.first())
+        else -> mutableListOf<E>().apply {
+            add(list.first())
+            addAll(take(count - 1, list.subList(1, list.size)))
         }
     }
 }
-
-class EmptyListException: RuntimeException()
--- lib/src/test/kotlin/study/kotlin/fp/RecursionTest.kt
+++ lib/src/test/kotlin/study/kotlin/fp/RecursionTest.kt
@@ -1,6 +1,7 @@
 package study.kotlin.fp
 
-import study.kotlin.fp.Recursion.max
+import study.kotlin.fp.Recursion.maximum
+import study.kotlin.fp.Recursion.reverse
 import java.math.BigInteger
 import kotlin.test.Ignore
 import kotlin.test.Test
@@ -38,10 +39,36 @@ class RecursionTest {
     }
     
     @Test
-    fun `list max success`() {
-        assertEquals(7, listOf(1, 2, 5, 6, 7, 2, 3, 4, 5, 6).max())
-        assertEquals(76, listOf(76, 23, 46, 76, 23, 1, 0).max())
-        assertEquals(3, listOf(3).max())
-        assertFailsWith<EmptyListException> { listOf<Int>().max() }
+    fun `list maximum success`() {
+        assertEquals(7, listOf(1, 2, 5, 6, 7, 2, 3, 4, 5, 6).maximum())
+        assertEquals(76, listOf(76, 23, 46, 76, 23, 1, 0).maximum())
+        assertEquals(3, listOf(3).maximum())
+        assertFailsWith<IllegalStateException> { listOf<Int>().maximum() }
+    }
+    
+    @Test
+    fun `list reverse success`() {
+        assertEquals(listOf("C", "D", "A", "E", "A"), listOf("A", "E", "A", "D", "C").reverse())
+        assertEquals(listOf(0, 9, 8, 7, 6, 5, 4, 3, 2, 1), listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 0).reverse())
+    }
+    
+    @Test
+    fun `convertDecimalToBinaryString success`() {
+        assertEquals("11", Recursion.convertDecimalToBinaryString(3))
+        assertEquals("1001111001011111001", Recursion.convertDecimalToBinaryString(324345))
+    }
+    
+    @Test
+    fun `replicate success`() {
+        assertEquals(listOf(5, 5, 5), Recursion.replicate(3, 5))
+        assertEquals(listOf(88, 88, 88, 88, 88, 88), Recursion.replicate(6, 88))
+    }
+    
+    @Test
+    fun `take success`() {
+        assertEquals(listOf("가", "나", "다"), Recursion.take(3, listOf("가", "나", "다", "라", "마", "바")))
+        assertEquals(listOf("가"), Recursion.take(1, listOf("가", "나", "다", "라", "마", "바")))
+        assertEquals(listOf(), Recursion.take(0, listOf("가", "나", "다", "라", "마", "바")))
+        assertEquals(listOf(55, 24, 23, 65, 34, 65, 34, 12, 34), Recursion.take(9, listOf(55, 24, 23, 65, 34, 65, 34, 12, 34, 23)))
     }
 }
