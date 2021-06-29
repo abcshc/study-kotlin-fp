172f29fdab148f3729727fb1e5790f27863abb95
Heechul So <thrrkthrrk@gmail.com>
2021-06-30 08:23:23 +0900

    Add tail recursive tests

--- lib/src/main/kotlin/study/kotlin/fp/TailRecursive.kt
+++ lib/src/main/kotlin/study/kotlin/fp/TailRecursive.kt
@@ -1,6 +1,18 @@
 package study.kotlin.fp
 
+import java.math.BigInteger
+
 object TailRecursive {
+    tailrec fun fibonacciNumbers(
+        n: Int,
+        acc1: BigInteger = BigInteger.ZERO,
+        acc2: BigInteger = BigInteger.ONE
+    ): BigInteger = when (n) {
+        0 -> acc1
+        1 -> acc2
+        else -> fibonacciNumbers(n - 1, acc2, acc1 + acc2)
+    }
+    
     tailrec fun factorial(n: Int, acc: Int = 1): Int = when (n) {
         0 -> acc
         else -> factorial(n - 1, acc * n)
@@ -9,5 +21,17 @@ object TailRecursive {
     tailrec fun power(base: Int, exponent: Int, acc: Int = 1): Int = when (exponent) {
         0 -> acc
         else -> power(base, exponent - 1, base * acc)
+    }
+    
+    tailrec fun List<Int>.maximum(value: Int = 0): Int = when (this.size) {
+        0 -> error("empty list")
+        1 -> this.first()
+        else -> this.subList(1, this.size).maximum(kotlin.math.max(value, this[0]))
+    }
+    
+    tailrec fun <E> List<E>.reverse(acc: List<E> = emptyList()): List<E> = when (this.size) {
+        0 -> emptyList()
+        1 -> acc + this
+        else -> this@reverse.subList(0, this@reverse.size - 1).reverse(acc)
     }
 }
--- lib/src/test/kotlin/study/kotlin/fp/TailRecursiveTest.kt
+++ lib/src/test/kotlin/study/kotlin/fp/TailRecursiveTest.kt
@@ -1,9 +1,27 @@
 package study.kotlin.fp
 
 import org.junit.Test
+import study.kotlin.fp.Recursion.maximum
+import study.kotlin.fp.Recursion.reverse
+import java.math.BigInteger
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 internal class TailRecursiveTest {
+    @Test
+    fun `fibonacciNumbers success`() {
+        val result = listOf(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377)
+        
+        for (i in 0..14) {
+            assertEquals(BigInteger.valueOf(result[i].toLong()), TailRecursive.fibonacciNumbers(i))
+        }
+    }
+    
+    @Test
+    fun `fibonacciNumbers big number`() {
+        assertEquals(BigInteger("9969216677189303386214405760200"), TailRecursive.fibonacciNumbers(150))
+    }
+    
     @Test
     fun `factorial success`() {
         assertEquals(1, TailRecursive.factorial(1))
@@ -16,5 +34,19 @@ internal class TailRecursiveTest {
         assertEquals(1024, TailRecursive.power(2, 10))
         assertEquals(32768, TailRecursive.power(2, 15))
         assertEquals(1000000000, TailRecursive.power(10, 9))
+    }
+    
+    @Test
+    fun `list maximum success`() {
+        assertEquals(7, listOf(1, 2, 5, 6, 7, 2, 3, 4, 5, 6).maximum())
+        assertEquals(76, listOf(76, 23, 46, 76, 23, 1, 0).maximum())
+        assertEquals(3, listOf(3).maximum())
+        assertFailsWith<IllegalStateException> { listOf<Int>().maximum() }
+    }
+    
+    @Test
+    fun `list reverse success`() {
+        assertEquals(listOf("C", "D", "A", "E", "A"), listOf("A", "E", "A", "D", "C").reverse())
+        assertEquals(listOf(0, 9, 8, 7, 6, 5, 4, 3, 2, 1), listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 0).reverse())
     }
 }
