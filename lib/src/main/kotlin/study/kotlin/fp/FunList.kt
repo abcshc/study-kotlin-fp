f32284f3a26a1f49fb1ca4947d70377bc922241d
Heechul So <thrrkthrrk@gmail.com>
2021-08-03 07:58:56 +0900

    Add addHead, appendTail, reverse tests in FunList

--- lib/src/main/kotlin/study/kotlin/fp/FunList.kt
+++ lib/src/main/kotlin/study/kotlin/fp/FunList.kt
@@ -4,3 +4,15 @@ sealed class FunList<out T> {
     object Nil: FunList<Nothing>()
     data class Cons<out T>(val head: T, val tail: FunList<T>): FunList<T>()
 }
+
+fun <T> FunList<T>.addHead(head: T): FunList<T> = FunList.Cons(head, this)
+
+tailrec fun <T> FunList<T>.appendTail(value: T, acc: FunList<T> = FunList.Nil): FunList<T> = when (this) {
+    FunList.Nil -> FunList.Cons(value, acc).reverse()
+    is FunList.Cons -> tail.appendTail(value, acc.addHead(head))
+}
+
+tailrec fun <T> FunList<T>.reverse(acc: FunList<T> = FunList.Nil): FunList<T> = when (this) {
+    FunList.Nil -> acc
+    is FunList.Cons -> tail.reverse(acc.addHead(head))
+}
--- lib/src/test/kotlin/study/kotlin/fp/FunListTest.kt
+++ lib/src/test/kotlin/study/kotlin/fp/FunListTest.kt
@@ -19,4 +19,28 @@ internal class FunListTest {
         val list: FunList<Double> = Cons(1.0, Cons(2.0, Cons(3.0, Cons(4.0, Cons(5.0, Nil)))))
         assertEquals(1.0, (list as Cons).head)
     }
+    
+    @Test
+    fun `addHead success`() {
+        val list = Cons(1, Nil).addHead(3) as Cons<Int>
+        
+        assertEquals(3, list.head)
+        assertEquals(1, (list.tail as Cons).head)
+    }
+    
+    @Test
+    fun `appendTail success`() {
+        val list = Cons(1, Nil).appendTail(3) as Cons<Int>
+        
+        assertEquals(1, list.head)
+        assertEquals(3, (list.tail as Cons).head)
+    }
+    
+    @Test
+    fun `reverse success`() {
+        val list = Cons(1, Cons(2, Nil)).reverse() as Cons<Int>
+        
+        assertEquals(2, list.head)
+        assertEquals(1, (list.tail as Cons).head)
+    }
 }
